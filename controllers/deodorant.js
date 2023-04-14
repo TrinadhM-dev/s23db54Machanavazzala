3dd77d137a8f72de28bb820fee360028051c137e
TrinadhM-dev <79310517+TrinadhM-dev@users.noreply.github.com>
2023-04-14 11:18:19 -0500

    get and put methods

--- controllers/deodorant.js
+++ controllers/deodorant.js
@@ -3,46 +3,46 @@ var Deodorant = require('../models/deodorants');
 
 // List of all Costumes
 exports.deodorant_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Deodorant list');
+    res.send('NOT IMPLEMENTED: Deodorant list');
 };
 // for a specific Costume.
-exports.deodorant_detail = function(req, res) {
+exports.Deodorant_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Deodorant detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
 exports.deodorant_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Deodorant create POST');
+    res.send('NOT IMPLEMENTED: Deodorant create POST');
 };
 // Handle Costume delete form on DELETE.
 exports.deodorant_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Deodorant delete DELETE ' + req.params.id);
+    res.send('NOT IMPLEMENTED: Deodorant delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
 exports.deodorant_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Deodorant update PUT' + req.params.id);
+    res.send('NOT IMPLEMENTED: Deodorant update PUT' + req.params.id);
 };
 // List of all Costumes
 exports.deodorant_list = async function(req, res) {
-try{
-theD = await Deodorant.find();
-res.send(theD);
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+    try{
+        theD = await Deodorant.find();
+        res.send(theD);
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // VIEWS
 // Handle a show all view
 exports.Deodorant_view_all_Page = async function(req, res) {
-try{
-theDeodorant = await Deodorant.find();
-res.render('deodorant', { title: 'Deodorant Search Results', results: theDeodorant });
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+    try{
+        theDeodorant = await Deodorant.find();
+        res.render('deodorant', { title: 'Deodorant Search Results', results: theDeodorant });
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 }
 
 //POST
@@ -54,11 +54,44 @@ exports.deodorant_create_post = async function(req, res) {
     document.D_Type = req.body.D_Type;
     document.D_Cost = req.body.D_Cost;
     try{
-    let result = await document.save();
-    res.send(result);
+        let result = await document.save();
+        res.send(result);
     }
     catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
+}
+// for a specific Costume.
+exports.Deodorant_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Deodorant.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
     }
+    };
+// Handle Costume update form on PUT.
+exports.deodorant_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Deodorant.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Deodorant_type)
+    toUpdate.Deodorant_type = req.body.Deodorant_type;
+    if(req.body.D_Name) toUpdate.D_Name = req.body.D_Name;
+    if(req.body.D_Type) toUpdate.D_Type = req.body.D_Type;
+    if(req.body.D_Cost) toUpdate.D_Cost = req.body.D_Cost;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+    
