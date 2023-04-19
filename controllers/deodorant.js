53b1dc498954af0a4b20ac1e19b79594f7c459a0
TrinadhM-dev <79310517+TrinadhM-dev@users.noreply.github.com>
2023-04-18 19:43:46 -0500

    Update/Delete Functionalities

--- controllers/deodorant.js
+++ controllers/deodorant.js
@@ -132,4 +132,30 @@ exports.deodorant_create_Page = function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
-
+// Handle building the view for updating a costume.
+// query provides the id
+exports.deodorant_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Deodorant.findById(req.query.id)
+    res.render('deodorantupdate', { title: 'Deodorant Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+// Handle a delete one view with id from query
+exports.deodorant_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Deodorant.findById(req.query.id)
+    res.render('deodorantdelete', { title: 'Deodorant Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/Deodorant.js
+++ routes/Deodorant.js
@@ -5,7 +5,13 @@ var Deodorant_controlers= require('../controllers/deodorant');
 /* GET costumes */
 router.get('/', Deodorant_controlers.Deodorant_view_all_Page );
 router.get('/detail', Deodorant_controlers.Deodorant_view_one_Page);
-/* GET create deodorant page */
+/* create create deodorant page */
 router.get('/create', Deodorant_controlers.deodorant_create_Page)
+/* update create update page */
+router.get('/update', Deodorant_controlers.deodorant_update_Page);
+/*  delete costume page */
+router.get('/delete', Deodorant_controlers.deodorant_delete_Page);
+
+
 module.exports = router;
 
