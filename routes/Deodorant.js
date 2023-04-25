555e3a3722d4c2687cdf09c1f5c1ad8797fa4a1d
TrinadhM-dev <79310517+TrinadhM-dev@users.noreply.github.com>
2023-04-25 13:21:19 -0500

    security given for delete and create

--- routes/Deodorant.js
+++ routes/Deodorant.js
@@ -16,11 +16,11 @@ res.redirect("/login");
 router.get('/', Deodorant_controlers.Deodorant_view_all_Page );
 router.get('/detail', Deodorant_controlers.Deodorant_view_one_Page);
 /* create create deodorant page */
-router.get('/create', Deodorant_controlers.deodorant_create_Page)
+router.get('/create',secured, Deodorant_controlers.deodorant_create_Page)
 /* update create update page */
 //router.get('/update',Deodorant_controlers.deodorant_update_Page);
 /*  delete costume page */
-router.get('/delete', Deodorant_controlers.deodorant_delete_Page);
+router.get('/delete',secured, Deodorant_controlers.deodorant_delete_Page);
 
 // /* GET update costume page */
 router.get('/update',secured,Deodorant_controlers.deodorant_update_Page);
